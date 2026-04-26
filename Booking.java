4278f6236cc0daa055849dd67093aaa6ee634333
swatibrathod0706 <swatirathod0706@gmail.com>
2026-04-25 22:34:34 -0700

    Create Booking.java

---
+++ Booking.java
@@ -0,0 +1,13 @@
+public class Booking {
+    int id;
+    String name;
+    int roomNo;
+    double amount;
+
+    public Booking(int id, String name, int roomNo, double amount) {
+        this.id = id;
+        this.name = name;
+        this.roomNo = roomNo;
+        this.amount = amount;
+    }
+}
